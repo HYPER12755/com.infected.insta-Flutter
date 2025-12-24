32154737b03126f62acf88b87c3129af0a083e89
hyper14917374 <hyper14917374@gmail.com>
2025-12-24 15:44:53 +0000

    Changes before Firebase Studio auto-run

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'features/auth/providers/auth_provider.dart';
-import 'features/auth/screens/login_screen.dart';
-import 'features/home/screens/home_screen.dart';
-import 'features/splash/screens/splash_screen.dart';
+import 'features/auth/presentation/auth_gate.dart';
 import 'firebase_options.dart';
 import 'theme/theme.dart';
 
@@ -17,31 +14,16 @@ void main() async {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends ConsumerWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final authState = ref.watch(authStateChangesProvider);
-
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Instagram Clone',
       theme: lightTheme,
       darkTheme: darkTheme,
-      home: authState.when(
-        data: (user) {
-          if (user != null) {
-            return const HomeScreen();
-          }
-          return const LoginScreen();
-        },
-        loading: () => const SplashScreen(),
-        error: (error, stackTrace) => Scaffold(
-          body: Center(
-            child: Text('Error: $error'),
-          ),
-        ),
-      ),
+      home: const AuthGate(),
     );
   }
 }
